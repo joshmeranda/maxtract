e126ce5a051e80615cc569557e31bcf7dee42dbf
joshmeranda <joshmeranda@gmail.com>
2021-10-19 22:56:58 -0400

    add initial mapping loginc

--- main.go
+++ main.go
@@ -3,12 +3,51 @@ package main
 import (
 	"fmt"
 	"github.com/akamensky/argparse"
+	"github.com/gocolly/colly"
+	"net/url"
 	"os"
 	"regexp"
 )
 
+func collect(url *url.URL, maxDepth int) {
+	fmt.Println("=== 000", url.Hostname(), "===")
+
+	filterRegex := regexp.MustCompile(fmt.Sprintf(".*%s.*", url.Hostname()))
+
+	collector := colly.NewCollector(
+		colly.URLFilters(filterRegex),
+		colly.MaxDepth(maxDepth),
+		colly.Async(true))
+
+	collector.OnHTML("a[href]", func(element *colly.HTMLElement) {
+		link := element.Attr("href")
+
+		// todo: do some processing
+
+		err := collector.Visit(link)
+
+		if err != nil {
+			// do nothing...
+		}
+	})
+
+	collector.OnRequest(func(request *colly.Request) {
+		fmt.Printf("=== 001 '%s' ===\n", request.URL)
+	})
+
+	err := collector.Visit(url.String())
+
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	collector.Wait()
+}
+
 func main() {
 	parser := argparse.NewParser("maxtract", "A command line tool for extracting information from websites")
+
+	parser.ExitOnHelp(true)
 
 	maxDepth := parser.Int("d", "max-depth", &argparse.Options{
 		Validate: func(args []string) error {
@@ -18,8 +57,8 @@ func main() {
 
 			return nil
 		},
-		Help:    "the maximum depth of links to walk down (must be >= 0). If 0 only the content at the given url will be search",
-		Default: -1,
+		Help:    "the maximum depth of links to walk down (must be >= 0) if 0 (the default) there will be not depth limit",
+		Default: 0,
 	})
 
 	dataOnly := parser.Flag("o", "data-only", &argparse.Options{
@@ -36,7 +75,6 @@ func main() {
 	})
 
 	var regex *regexp.Regexp
-
 	_ = parser.String("p", "pattern", &argparse.Options{
 		Required: true,
 		Validate: func(args []string) (err error) {
@@ -54,19 +92,35 @@ func main() {
 		Help: "the pattern to search for, can be one of phone, email, or a custom regex pattern",
 	})
 
-	url := parser.String("u", "url", &argparse.Options{
+	var rootUrl *url.URL
+	parser.String("u", "url", &argparse.Options{
 		Required: true,
-		Help:     "the url at which to start the search",
+		Validate: func(args []string) (err error) {
+			parsedUrl, err := url.Parse(args[0])
+
+			rootUrl = parsedUrl
+
+			return err
+		},
+		Help: "the url at which to start the search",
 	})
 
 	err := parser.Parse(os.Args)
 
 	if err != nil {
 		fmt.Println("Error:", err)
+		return
+	}
+
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else if true {
 	} else {
 		fmt.Printf("=== %t %t %t %t ===\n", *dataOnly, *full, *json, *prettyJson)
 		fmt.Printf("=== %d ===\n", *maxDepth)
-		fmt.Printf("=== %s ===\n", *url)
+		fmt.Printf("=== %s ===\n", rootUrl.String())
 		fmt.Printf("=== %s ===\n", regex.String())
 	}
+
+	collect(rootUrl, *maxDepth)
 }
